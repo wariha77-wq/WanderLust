a35776a6bb3191cd64f6230ad087993983bd1a04
Wariha Tariq <wariha777@gmail.com>
2026-08-03 13:29:03 -0700

    Adding marker icon

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,23 @@
 
   mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
-        
+       
         container: 'map', // container ID
         center: listing.geometry.coordinates , // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
 console.log(listing.geometry.coordinates);
-    
- const marker = new mapboxgl.Marker({color:"red"})
+const el = document.createElement("div");
+el.className = "marker-container";
+
+el.innerHTML = `
+    <div class="marker-radius"></div>
+    <div class= "custom-marker"> 
+     <i class="fa-solid fa-house"></i>
+    </div> 
+`;    
+const marker = new mapboxgl.Marker(el)
         .setLngLat(listing.geometry.coordinates)
         .setPopup(new mapboxgl.Popup({offset: 25})
         .setHTML(`<h4>${listing.title}</h4><p>Exact location provided after booking!</p>`))
